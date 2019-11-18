5d1136c24ee70d2dab5ad0d506826774a7cc336a
HajimeNoJosh <joshuah.m.martin@gmail.com>
2019-11-18 17:43:34 -0500

    Change style
    1. Add degree to degrees
    2. Add space to comma
    3. Show error and success messages

--- assets/scripts/countries/ui.js
+++ assets/scripts/countries/ui.js
@@ -1,13 +1,15 @@
-
 const onSuccess = function (data) {
-  $('.location-fill').html(data.name + ',' + data.sys.country)
-  $('.temperature-fill').html(Math.floor(data.main.temp * 9 / 5 - 459.67))
-  $('.humidity-fill').html(data.main.humidity)
+  $('.location-fill').html(data.name + ', ' + data.sys.country)
+  $('.temperature-fill').html(Math.floor(data.main.temp * 9 / 5 - 459.67) + '° F')
+  $('.humidity-fill').html(data.main.humidity + '%')
   $('.conditions-fill').html(data.weather[0].main)
+  $('.form-field').val('')
+  $('.message').show().text('Showing Country').css('color', '#024b30').hide(2000)
+  $('.focus').focus()
 }
 
 const onFailure = function () {
-
+  $('.message').show().text('Could not find country').css('color', 'red').hide(2000)
 }
 
 module.exports = {
